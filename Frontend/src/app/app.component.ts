e9792f7e1145ad75f95178f2cc584792f9616c1d
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-11-02 05:17:02 +0100

    Connected frontend to backend, giggidy

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/services/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+boxName: any;
+writeBoxName: any;
+
+constructor(private http: HttpService)
+  {
+      
+  }
+
+  async ngOnInit() {
+
+    boxName: string = "";
+
+    const boxes = await this.http.getBoxes();
+    console.log(boxes);
+  }
+
+  writeBoxName() {
+    console.log(this.boxName);
+  }
+
 }
